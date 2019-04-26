d6153dd1d5d81de0e29953787e1653492adc034d
shawphon <49856234+shawphon@users.noreply.github.com>
2019-04-26 09:48:54 +0800

    Add files via upload

---
+++ packages/basic/get_bfunctions_path.py
@@ -0,0 +1,6 @@
+import os
+
+def Get_Bfunctions_Path():
+	"""得到本模块的绝对路径"""
+	
+	return os.path.abspath('.')
---
+++ packages/for_file/get_ffunctions_path.py
@@ -0,0 +1,6 @@
+import os
+
+def Get_Ffunctions_Path():
+	"""得到本模块的绝对路径"""
+	
+	return os.path.abspath('.')
